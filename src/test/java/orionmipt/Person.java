ddb552ddf49fffc8f14fbbf167940311d37fd632
OrionChocoPie <nogayev.nuras@gmail.com>
2020-09-21 14:50:17 +0600

    Just off warnings in Person in test

--- src/test/java/orionmipt/Person.java
+++ src/test/java/orionmipt/Person.java
@@ -6,8 +6,32 @@ class Person {
     private int age;
 
     public Person(String n, boolean m, int a) {
-        name = n;
-        isMale = m;
-        age = a;
+        setName(n);
+        setMale(m);
+        setAge(a);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
