eff09f6cd079032c8ded38445be1603694f2662b
Ivan Arkhipov <e1.gildor@gmail.com>
2021-04-26 15:19:10 +0200

    Added base Introspection interface

---
+++ ljv/src/main/java/org/atpfivt/ljv/Introspection.java
@@ -0,0 +1,15 @@
+package org.atpfivt.ljv;
+
+public interface Introspection {
+
+    public String getObjClassName(Object obj, boolean useToStringAsClassName);
+
+    public boolean canTreatObjAsPrimitive(Object obj);
+
+    public boolean catTreatObjAsArrayOfPrimitives(Object obj);
+
+    public boolean canTreatClassAsPrimitive(Class<?> cz);
+
+    public boolean hasPrimitiveObjectFields(Object obj);
+
+}
