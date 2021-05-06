40df8e8d828992700e85a6e6d0add6f9718a5e33
Ivan Arkhipov <e1.gildor@gmail.com>
2021-05-06 21:25:59 +0200

    Switched GraphBuilder to use IntrospectionWithReflectionAPI class instead of internal logic and ObjSettings. Removed ObjSettings

--- ljv/src/main/java/org/atpfivt/ljv/GraphBuilder.java
+++ ljv/src/main/java/org/atpfivt/ljv/GraphBuilder.java
@@ -1,22 +1,19 @@
 package org.atpfivt.ljv;
 
-import org.reflections.ReflectionUtils;
-
-import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.IdentityHashMap;
 
 final class GraphBuilder {
     private final IdentityHashMap<Object, String> objectsId = new IdentityHashMap<>();
-    private final ObjSettings oSettings;
     private final StringBuilder out = new StringBuilder();
     private final LJV ljv;
+    private final Introspection introspection;
     private boolean nullNode;
 
     public GraphBuilder(LJV ljv) {
         this.ljv = ljv;
-        this.oSettings = new ObjSettings(ljv);
+        this.introspection = new IntrospectionWithReflectionAPI(ljv);
     }
 
     private String dotName(Object obj) {
@@ -69,37 +66,25 @@ final class GraphBuilder {
         }
     }
 
-    private int getFieldSize(LJV ljv, Object obj, Field[] fs) {
-        int size = 0;
-        for (Field field : fs) {
-            if (!ljv.canIgnoreField(field))
-                try {
-                    Object ref = field.get(obj);
-                    if (field.getType().isPrimitive() || oSettings.canTreatAsPrimitive(ref))
-                        size++;
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-        }
-        return size;
-    }
+    private void labelObjectWithSomePrimitiveFields(Object obj) {
+        Field fields[] = introspection.getObjFields(obj);
 
-    private void labelObjectWithSomePrimitiveFields(Object obj, Field[] fs) {
         out.append("\t")
                 .append(dotName(obj))
                 .append("[label=<\n")
                 .append("\t\t<table border='0' cellborder='1' cellspacing='0'>\n")
                 .append("\t\t\t<tr>\n\t\t\t\t<td rowspan='")
-                .append(getFieldSize(ljv, obj, fs) + 1)
+                .append(introspection.countObjectPrimitiveFields(obj) + 1)
                 .append("'>")
-                .append(oSettings.className(obj, false))
+                .append(introspection.getObjClassName(obj, false))
                 .append("</td>\n\t\t\t</tr>\n");
+
         String cabs = ljv.getObjectAttributes(obj);
-        for (Field field : fs) {
+        for (Field field : fields) {
             if (!ljv.canIgnoreField(field))
                 try {
                     Object ref = field.get(obj);
-                    if (field.getType().isPrimitive() || oSettings.canTreatAsPrimitive(ref)) {
+                    if (field.getType().isPrimitive() || introspection.canTreatObjAsPrimitive(ref)) {
                         out.append("\t\t\t<tr>\n\t\t\t\t<td>");
                         if (ljv.isShowFieldNamesInLabels())
                             out.append(field.getName()).append(": ").append(Quote.quote(String.valueOf(ref)));
@@ -124,18 +109,20 @@ final class GraphBuilder {
                 .append("[label=<\n")
                 .append("\t\t<table border='0' cellborder='1' cellspacing='0'>\n")
                 .append("\t\t\t<tr>\n\t\t\t\t<td>")
-                .append(oSettings.className(obj, false))
+                .append(introspection.getObjClassName(obj, false))
                 .append("</td>\n\t\t\t</tr>\n\t\t</table>\n\t>")
                 .append(cabs.isEmpty() ? "" : "," + cabs)
                 .append("];\n");
     }
 
-    private void processFields(Object obj, Field[] fs) {
+    private void processFields(Object obj) {
+        Field[] fs = introspection.getObjFields(obj);
+
         for (Field field : fs) {
             if (!ljv.canIgnoreField(field)) {
                 try {
                     Object ref = field.get(obj);
-                    if (field.getType().isPrimitive() || oSettings.canTreatAsPrimitive(ref))
+                    if (field.getType().isPrimitive() || introspection.canTreatObjAsPrimitive(ref))
                         //- The field might be declared, say, Object, but the actual
                         //- object may be, say, a String.
                         continue;
@@ -167,61 +154,19 @@ final class GraphBuilder {
         } else if (!objectsId.containsKey(obj)) {
             Class<?> c = obj.getClass();
             if (c.isArray()) {
-                if (oSettings.looksLikePrimitiveArray(obj))
+                if (introspection.catTreatObjAsArrayOfPrimitives(obj))
                     processPrimitiveArray(obj);
                 else
                     processObjectArray(obj);
             } else {
-                Field[] fs = ReflectionUtils.getAllFields(c,
-                        f -> {
-                            if (ljv.isIgnoreNullValuedFields()) {
-                                try {
-                                    f.setAccessible(true);
-                                    return f.get(obj) != null;
-                                } catch (IllegalAccessException e) {
-                                    return false;
-                                }
-                            }
-                            return true;
-                        }
-                ).toArray(new Field[0]);
 
-                normalizeFieldsOrder(fs);
-                if (!ljv.isIgnorePrivateFields())
-                    AccessibleObject.setAccessible(fs, true);
-
-                if (oSettings.hasPrimitiveFields(fs, obj))
-                    labelObjectWithSomePrimitiveFields(obj, fs);
+                if (introspection.hasPrimitiveFields(obj))
+                    labelObjectWithSomePrimitiveFields(obj);
                 else
                     labelObjectWithNoPrimitiveFields(obj);
 
-                processFields(obj, fs);
+                processFields(obj);
             }
-        }
-    }
-
-    private static void normalizeFieldsOrder(Field[] fs) {
-        /*Ensure that 'left' field is always processed before 'right'.
-        The problem is that ReflectionUtils.getAllFields uses HashSet, not LinkedHashSet,
-        and loses information about fields order.
-
-        This is a hard-coded logic and should be removed in the future.
-         */
-        int i = 0, left = -1, right = -1;
-        for (Field f : fs) {
-            if ("left".equals(f.getName())) {
-                left = i;
-                break;
-            } else if ("right".equals(f.getName())) {
-                right = i;
-            }
-            i++;
-        }
-        if (right > -1 && left > right) {
-            //swap left & right
-            Field f = fs[left];
-            fs[left] = fs[right];
-            fs[right] = f;
         }
     }
 
