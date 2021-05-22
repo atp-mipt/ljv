a19e86102573ef26a75b0a4f7ba168c06d2eb018
Ivan Arkhipov <e1.gildor@gmail.com>
2021-05-22 08:05:47 +0200

    Implemented common visualization

--- ljv/src/main/java/org/atpfivt/ljv/Visualization.java
+++ ljv/src/main/java/org/atpfivt/ljv/Visualization.java
@@ -1,19 +1,21 @@
 package org.atpfivt.ljv;
 
-import java.lang.reflect.Field;
-
 
 public interface Visualization {
     public void beginDOT();
     public void finishDOT();
 
-    public void visitArrayBegin(Object array);
-    public void visitArrayElement(Object array, Object elem);
+    public void visitNull();
+
+    public void visitArrayBegin(Object array, boolean hasPrimitiveValues);
+    public void visitArrayElement(Object array, Object element, int elementIndex, boolean isPrimitive);
+    public void visitArrayElementObjectConnection(Object array, int elementIndex, Object obj);
     public void visitArrayEnd(Object array);
 
-    public void visitObjectBegin(Object obj);
-    public void visitObjectField(Object obj, Field field);
+    public void visitObjectBegin(Object obj, String className, int primitiveFieldsNum);
+    public void visitObjectPrimitiveField(Object obj, String fieldName, String fieldValueStr);
     public void visitObjectEnd(Object obj);
+    public void visitObjectFieldRelationWithNonPrimitiveObject(Object obj, String fieldName, String ljvFieldAttributes, Object relatedObject);
 
     public String generateResult();
 }
